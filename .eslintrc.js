48e0bcb129de3281053884cc2a73604af2c59f73
joel <bonjourjoel@gmail.com>
2024-10-08 16:54:55 +0200

    add promise check in eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -20,5 +20,13 @@ module.exports = {
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-floating-promises': 'error',
+    '@typescript-eslint/no-misused-promises': [
+      'error',
+      {
+        checksConditionals: true, // Prevents the use of a promise in a conditional like if/else
+        checksVoidReturn: false, // Allows async functions used in contexts where their return value is ignored
+      },
+    ],
   },
 };
