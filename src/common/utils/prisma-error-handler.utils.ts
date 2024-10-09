f958326712e3064b064ccc2b6fa58fbc64660491
joel <bonjourjoel@gmail.com>
2024-10-09 21:11:56 +0200

    improve prismaErrorMiddleware to return the error message

--- src/common/utils/prisma-error-handler.utils.ts
+++ src/common/utils/prisma-error-handler.utils.ts
@@ -1,13 +1,21 @@
-import { NotFoundException } from '@nestjs/common';
+import {
+  InternalServerErrorException,
+  NotFoundException,
+} from '@nestjs/common';
 
 enum PRISMA_ERROR_CODE {
   RECORD_NOT_FOUND = 'P2025',
 }
 
 export function prismaErrorMiddleware(error: any): any {
+  // format the error message to return the prisma error in the http error
+  const errMsg: string = error.message || 'Unknown prisma error';
+  // handle the known errors appriopriately
   if (error.code === PRISMA_ERROR_CODE.RECORD_NOT_FOUND) {
-    return new NotFoundException();
+    return new NotFoundException(errMsg);
   } else {
-    return error;
+    // format the unknown errors properly to return the prisma error in the http error
+    const errMsg: string = error.message || 'Unknown prisma error';
+    return new InternalServerErrorException(errMsg);
   }
 }
